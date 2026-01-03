7dfb917a5f14a54d2739d7124941dd756e20e1fe
MAINGOAL426 <nzeziaz@gmail.com>
2026-01-02 22:38:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function setPayment(network, address, amount) {
+  const box = document.getElementById("payment-box");
+  box.innerHTML = `
+    <b>Network:</b> ${network}<br><br>
+    <b>Amount:</b> ${amount}<br><br>
+    <b>Send to address:</b><br>
+    <span class="address">${address}</span><br><br>
+    <small>After payment confirmation, NFT will be transferred manually.</small>
+  `;
+}
