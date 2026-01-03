86527359ae5613f7cc01a9e159d817e1c68fb137
MAINGOAL426 <nzeziaz@gmail.com>
2026-01-03 10:06:14 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function setPayment(network, address, amount, note) {
   const box = document.getElementById("payment-box");
   box.innerHTML = `
-    <b>Network:</b> ${network}<br><br>
-    <b>Amount:</b> ${amount}<br><br>
-    <b>Send to address:</b><br>
+    <b>Сеть:</b> ${network}<br><br>
+    <b>Сумма:</b> ${amount}<br><br>
+    <b>Отправьте на адрес:</b><br>
     <span class="address">${address}</span><br><br>
     <small>${note}</small>
   `;
 }
+
+
 
